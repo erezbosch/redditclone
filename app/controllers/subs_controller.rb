7362b455fb20a568a8824f8d794256f1d2a96c56
App Academy Student <student@appacademy.io>
2015-07-17 13:10:39 -0700

    first commit

---
+++ app/controllers/subs_controller.rb
@@ -0,0 +1,51 @@
+class SubsController < ApplicationController
+  before_action :redirect_unless_moderator, only: [:edit, :update]
+
+  def index
+    @subs = Sub.all
+    render :index
+  end
+
+  def new
+    @sub = Sub.new
+    render :new
+  end
+
+  def create
+    @sub = current_user.subs.new(sub_params)
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+    if @sub.update(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
+
+  def redirect_unless_moderator
+    redirect_to subs_url unless current_user == Sub.find(params[:id]).moderator
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+  before_validation :ensure_session_token
+  validates :username, :password_digest, presence: true
+  validates :password, length: { minimum: 1, maximum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  has_many :subs
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(input)
+    @password = input
+    self.password_digest = BCrypt::Password.create(input)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(input)
+    BCrypt::Password.new(self.password_digest).is_password?(input)
+  end
+
+
+end
